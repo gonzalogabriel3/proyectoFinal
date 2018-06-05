6ae9be457f6bdce9d843742591026ca9ad2f3eb8
gonzalogabriel3 <ggcruzado@udc.edu.ar>
2018-06-05 01:09:29 -0300

    Agrego modelo/vistas y parte del crud de Usuario

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Usuario;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $usuarios=Usuario::all();
+        return view('usuario/index',compact('usuarios'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('/usuario/alta');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'nombre'        => 'required',
+            'email' => 'required',
+            'password'=>'required'
+        ]);
+
+        $usuario = new Usuario;
+        $usuario->nombre = $request->nombre;
+        $usuario->email = $request->email;
+        
+        //Realizo hash de la contraseña ingresada por el usuario
+        $pass=password_hash('{{$request->password}}',PASSWORD_DEFAULT)."\n";
+        $usuario->password = $pass;
+        $usuario->save();
+        
+        return redirect()->route('usuarios.index')->with("mensaje","Usuario registrado correctamente");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        dd($id);
+        $usuario=Usuario::find($id);
+        Usuario::destroy($usuario);
+        
+        return redirect()->route('usuarios.index')->with("mensaje","Usuario eliminado correctamente");
+    }
+}
