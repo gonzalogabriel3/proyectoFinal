b9585aa9e823fd6c8e3779575e55aeb363bcc68c
gonzalogabriel3 <ggcruzado@udc.edu.ar>
2018-08-01 11:45:40 -0300

    Subo cambios al componente de mapa

--- public/js/app.js
+++ public/js/app.js
@@ -53505,8 +53505,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
     data: function data() {
@@ -53516,30 +53514,24 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             paradas: [],
             recorridos: [],
             puntosRecorrido: [],
-            mapa: [] //Variable donde se va a contener el mapa principal
+            mapa: null //Variable donde se va a contener el mapa principal
         };
     },
     mounted: function mounted() {
         this.Leer();
-        this.mapa = this.GenerarMapa();
+        this.GenerarMapa();
     },
 
     methods: {
         GenerarMapa: function GenerarMapa() {
             /*Genero el mapa*/
-            var coordenadas_rawson = ol.proj.transform([-65.1056655, -43.2991348], 'EPSG:4326', 'EPSG:3857');
+            var coordenadas_rawson = [-43.2991348, -65.1056655];
+            this.mapa = L.map('mapid').setView(coordenadas_rawson, 12);
 
-            var mapa = new ol.Map({
-                layers: [new ol.layer.Tile({
-                    source: new ol.source.OSM()
-                })],
-                target: 'mapa',
-                view: new ol.View({
-                    center: coordenadas_rawson,
-                    zoom: 13
-                })
-            });
-            return mapa;
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 18,
+                id: 'mapbox.streets'
+            }).addTo(this.mapa);
         },
         BuscarRecorrido: function BuscarRecorrido(id) {
             var _this = this;
@@ -53549,8 +53541,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             axios.get("/mapa/" + this.recorrido_identificador).then(function (response) {
                 _this.puntosRecorrido = response.data.puntos;
             });
-            console.log(this.puntosRecorrido);
             this.closeModalRecorrido();
+            //this.puntosRecorrido=[[-43.3397844,-65.0585075],[-43.3398908,-65.0577351],[-43.3397519,-65.0571293],[-43.3396336,-65.0568447],[-43.3394532,-65.0565236],[-43.3391457,-65.0560845],[-43.3388914,-65.0557105],[-43.3386519,-65.055369],[-43.3383267,-65.0549136],[-43.3380812,-65.0545843],[-43.337895,-65.0543811],[-43.3378095,-65.0543046],[-43.3376646,-65.0542326],[-43.3375127,-65.0541774],[-43.3372386,-65.0540742],[-43.3370936,-65.0540526],[-43.3368771,-65.0540093],[-43.3367549,-65.0539877],[-43.3365506,-65.0539493],[-43.3363393,-65.0538773],[-43.3361997,-65.0538293],[-43.3360154,-65.0537681],[-43.3359212,-65.0537297],[-43.3357771,-65.0536817],[-43.3356304,-65.0536336],[-43.3354776,-65.0535808],[-43.335379,-65.053546],[-43.3352637,-65.053504],[-43.3351127,-65.0534488],[-43.3349407,-65.053384],[-43.3347853,-65.0533312],[-43.3346299,-65.0532759],[-43.3344789,-65.0532267],[-43.3343357,-65.0531583],[-43.334155,-65.0530923],[-43.3340781,-65.0530563],[-43.3339777,-65.0530215],[-43.3338948,-65.0529795],[-43.3337891,-65.0529603]];
             this.AgregarLineString();
         },
         Leer: function Leer() {
@@ -53569,62 +53561,11 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                 _this2.puntosRecorrido = response.data.puntos;
             });
         },
-
-        /*---AGREGAR UN MARCADOR AL MAPA---*/
         agregarMarcador: function agregarMarcador() {
-
-            var marcadores = []; //Creo un array donde van a ir todos los marcadores que quiero agregar al mapa
-
-            //Creo una nueva geometria donde defino las coordenadas,sistema de coordenadas,etc.
-            var iconFeature = new ol.Feature({
-                geometry: new ol.geom.Point(ol.proj.transform([-65.0521686, -43.3081713], 'EPSG:4326', 'EPSG:3857')),
-                name: 'Parada'
-            });
-
-            //Añado la geometria creada(punto)  al array de marcadores
-            marcadores.push(iconFeature);
-
-            //Fuentes
-            var vectorSource = new ol.source.Vector({
-                features: marcadores //add an array of features
-            });
-
-            //Defino el estilo del marcador
-            var iconStyle = new ol.style.Style({
-                image: new ol.style.Icon({
-                    opacity: 0.75,
-                    src: 'http://static.websguru.com.ar/var/m_a/a7/a78/32892/522624-icono-bus.png'
-                })
-            });
-
-            /*Creo una capa vectorial,indicando la fuente y el estilo para aplicar a dicha capa,
-            que posteriormente se agregara al mapa*/
-            var vectorLayer = new ol.layer.Vector({
-                source: vectorSource,
-                style: iconStyle
-            });
-
-            //Añado la capa vectorial al mapa
-            this.mapa.addLayer(vectorLayer);
-
-            /*---FIN AGREGAR MARCADOR---*/
+            var marker = L.marker([-43.3081713, -65.0521686]).addTo(this.mapa).bindPopup('<b>Usted esta aqui</b>').openPopup();
         },
         AgregarLineString: function AgregarLineString() {
-            /*--- INICIO LINESTRING ---*/
-            //Creo un Vector de Tipo LineString y le paso las Coordenadas
-            var layerLines = new ol.layer.Vector({
-                source: new ol.source.Vector({
-                    features: [new ol.Feature({
-                        geometry: new ol.geom.LineString(this.puntosRecorrido),
-                        name: 'Line'
-                    })]
-                })
-            });
-
-            //añado la capa del Linestring al mapa
-            this.mapa.addLayer(layerLines);
-
-            /*---- FIN DE LINESTRING ----*/
+            var polyline = L.polyline(this.puntosRecorrido, { color: 'blue' }).addTo(this.mapa);
         },
 
         //MODALS funciones
@@ -53647,8 +53588,8 @@ var render = function() {
   var _c = _vm._self._c || _h
   return _c("div", [
     _c("div", {
-      staticStyle: { width: "80%", height: "80%" },
-      attrs: { id: "mapa" }
+      staticStyle: { width: "1200px", height: "500px" },
+      attrs: { id: "mapid" }
     }),
     _vm._v(" "),
     _c(
